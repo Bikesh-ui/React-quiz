75de673d6652f0495599aeb3ea19b1f9d4248daa
bikesh thapa <thapabikesh051@gmail.com>
2021-07-30 21:47:54 +0545

    Updates

---
+++ precache-manifest.c4968a6ef198fbfdf5aca129b96af9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c23affc281d0f1abe8b0b3c9d40379",
+    "url": "/React-quiz/index.html"
+  },
+  {
+    "revision": "047a451e28e974745759",
+    "url": "/React-quiz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/React-quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047a451e28e974745759",
+    "url": "/React-quiz/static/js/main.be7f2b88.chunk.js"
+  },
+  {
+    "revision": "e9873c6a6c42c46c9442",
+    "url": "/React-quiz/static/js/runtime-main.b97b2b16.js"
+  }
+]);
